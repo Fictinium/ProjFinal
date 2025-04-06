be2c3435d483af7576eacacd4167866ab3eb4219
Guilherme L <101589883+Fictinium@users.noreply.github.com>
2025-04-06 17:36:07 +0100

    Configuração da base de dados#1

---
+++ ProjFinal/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjFinal.Data
+{
+    namespace YourProjectNamespace.Data
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+            {
+            }
+        }
+    }
+
+}
